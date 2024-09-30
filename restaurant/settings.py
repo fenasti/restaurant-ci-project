f151ff24a3f56d7a1960f5592b2978e7adc88146
Felipe Nast <td9z0vyv@students.codeinstitute.net>
2024-09-30 15:17:01 +0000

    readies code for deploy

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -25,9 +25,8 @@ SECRET_KEY = 'django-insecure-a8xcsyhu&^z^iis)&j-m83#65v2u6vam6x3jih*%=+5nhgeys4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    '8000-fenasti-restaurantcipro-bpyuv8fbuqd.ws.codeinstitute-ide.net',
-    '.herokuapp.com',
+ALLOWED_HOSTS = [ '8000-fenasti-restaurantcipro-bpyuv8fbuqd.ws.codeinstitute-ide.net',
+                '.herokuapp.com',
 ]
 
 
