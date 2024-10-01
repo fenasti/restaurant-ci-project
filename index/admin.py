0b771ee0df6c7758cd89c9642197eedfceb0be85
Felipe Nast <td9z0vyv@students.codeinstitute.net>
2024-10-01 00:51:48 +0000

    add summernote and reorganize models

--- index/admin.py
+++ index/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from .models import Home
+from django_summernote.admin import SummernoteModelAdmin
+
+@admin.register(Home)
+class HomeAdmin(SummernoteModelAdmin):
+
+    list_display = ('title','content')
+    search_fields = ['title']
+    summernote_fields = ('content',)
 
 # Register your models here.
-admin.site.register(Home)
+# admin.site.register(Home)
 
--- index/models.py
+++ index/models.py
@@ -7,7 +7,14 @@ class Home(models.Model):
     title = models.CharField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="owner")
     content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
 
 
 # featured_image = CloudinaryField('image', default='placeholder')
